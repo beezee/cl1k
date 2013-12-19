2ec01265a61ab0a127e814efd6177646bfe680ed
Ubuntu <ubuntu@ip-172-31-12-146.us-west-2.compute.internal>
2013-12-19 15:35:03 +0000

    initial commit

---
+++ config/initializers/clearance.rb
@@ -0,0 +1,5 @@
+Clearance.configure do |config|
+  config.mailer_sender = 'robot@cl1k.co'
+	config.cookie_domain = '.cl1k.co'
+	config.httponly = true
+end
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cl1k::Application.config.session_store :cookie_store, key: '_cl1k_session'
